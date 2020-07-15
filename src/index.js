066cd51a51b794624fc01df8e3cd2160e53f6c92
nnashiki <n.nashiki.work@gmail.com>
2020-07-15 15:25:29 +0900

    update: セルが編集できようになった

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 var columnDefs = [
-    {headerName: "Make", field: "make", sortable: true, filter: true},
-    {headerName: "Model", field: "model", sortable: true, filter: true},
-    {headerName: "Price", field: "price", sortable: true, filter: true}
+    {headerName: "Make", field: "make", sortable: true, filter: true, editable:true},
+    {headerName: "Model", field: "model", sortable: true, filter: true, editable:true},
+    {headerName: "Price", field: "price", sortable: true, filter: true, editable:true},
 ];
 
 // let the grid know which columns and what data to use
 var gridOptions = {
-    columnDefs: columnDefs
+    pagination: true,
+    columnDefs: columnDefs,
+    defaultColDef: {
+        flex: 1,
+        minWidth: 110,
+        editable: true,
+        resizable: true,
+    },
+    onCellEditingStarted: function (event) {
+
+        console.log('cellEditingStarted');
+    },
+    onCellEditingStopped: function (event) {
+        console.log('cellEditingStopped');
+    },
 };
 
 // setup the grid after the page has finished loading
